033ddcbe84a683e889e120e8d5f5fa55e20622bf
Esther Weon <esther.s.weon@gmail.com>
2018-03-13 12:13:36 -0700

    Did part i Exercises

--- js/turn_3rd_cell_blue.js
+++ js/turn_3rd_cell_blue.js
@@ -20,7 +20,7 @@
     'g' => green
 */
 
-var initial_state = ['', '.', '.', '.', '.'];
+var initial_state = [ '.', '.', '.', '.', '.' ];
 
 function main() {
   //////////////////////////////////
@@ -28,26 +28,10 @@ function main() {
   //// v YOUR CODE BELOW HERE v ////
   ////                          ////
   //////////////////////////////////
-
-  if (onBlue()) {
-    useBlue()
-  } else if (onGreen()) {
-    useGreen()
-  }
-
-  if (onBlue() || onGreen()) {
-    erase()
-    moveRight()
-    moveRight()
-    moveRight()
-    moveRight()
-    draw()
-  } else {
-    moveRight()
-    moveRight()
-    moveRight()
-    moveRight()
-  }
+  useBlue();
+  moveRight();
+  moveRight();
+  draw();
 
   //////////////////////////////////
   ////                          ////
